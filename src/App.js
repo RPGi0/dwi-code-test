4ab3876bfb8d1ed4f5fc1ba59d4ab705fc0116be
Rob <gio@llnnll.com>
2019-06-04 22:06:16 -0400

    finished wine offer listing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ProductListing from './ProductListing';
 import Alert from 'react-bootstrap/Alert';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
--- src/ProductListing.js
+++ src/ProductListing.js
@@ -1,41 +1,40 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-const ProductListing = (props) => {
+const ProductListing = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://www.wsjwine.com/api/offer/0033008',
+      );
+      setData(result.data.response.mainItems);
+    };
+    fetchData();
+  }, []);
+
+  console.log(data);
 
   return (
-    <div className='container'>
-      <div id="offer-content1-desktop" className="hidden-xs">
-        <div className="form-section">
-          <div className="section-header section-header-1">
+    <div id='prodList' className='row'>
+      <div id='offer-content1-desktop'>
+        <div className='form-section'>
+          <div className='section-header section-header-1'>
             <h3>Which Case Would You Like?</h3>
           </div>
-
           <p>
             Whatever you choose, we’ll add in two bonus Cabs and two crystal glasses and you’ll have the complete package – worth over $250 – for ONLY $69.99 (plus $19.99 shipping &amp; applicable tax; please
             allow 5-10 days for delivery, depending on shipping state).
           </p>
-
-          <div className="wine-select">
-            <label className="">
-              <input type="radio" className="radio" name="wineSelect" checked="" data-bom="A07616" value="all-reds" />
-                <b>Top 12 Reds</b> + 2 BONUS Bottles &amp; Glasses <b>JUST $69.99</b>
-                <a href="#" className="toggle-single-wine-modal" data-bom="A07616" data-title="Top 12 Reds"
-                   data-toggle="modal" data-target="#single-wine-modal" >view wines</a>
-            </label>
-
-            <label className="">
-              <input type="radio" className="radio" name="wineSelect" data-bom="A07617" value="all-whites" />
-                <b>Top 12 Whites</b> + 2 BONUS Bottles &amp; Glasses <b>JUST $69.99</b>
-                <a href="#" className="toggle-single-wine-modal" data-bom="A07617" data-title="Top 12 Whites"
-                   data-toggle="modal" data-target="#single-wine-modal">view wines</a>
-            </label>
-
-            <label className="">
-              <input type="radio" className="radio" name="wineSelect" data-bom="A07618" value="mixed" />
-                <b>Top 12 Mix</b> + 2 BONUS Bottles &amp; Glasses <b>JUST $69.99</b>
-                <a href="#" className="toggle-single-wine-modal" data-bom="A07618" data-title="Top 12 Mix"
-                   data-toggle="modal" data-target="#single-wine-modal" >view wines</a>
-            </label>
+          <div className='wine-select'>
+            {data.map((wine) =>
+              <div className='offerChoice' key={wine.itemCode}>
+              <input type='radio' className='radio' name='wineSelect' data-bom={wine.itemCode} value={wine.product.colourId} />
+              <b>{wine.product.name}</b> + 2 BONUS Bottles &amp; Glasses <b>JUST {wine.product.skus[0].salePrice}</b>
+              <a href='#' className='toggle-single-wine-modal' data-bom={wine.itemCode} data-title={wine.product.name}
+                 data-toggle='modal' data-target='#single-wine-modal' >view wines</a>
+            </div>)}
           </div>
         </div>
       </div>
